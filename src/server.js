ad86af88e1892a24ab10b0041a7ca646e8041b5e
Igor Novais <igorhnovaisdriven@gmail.com>
2022-12-07 23:10:29 -0300

    feat: server & db.js

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Running in port ${port}`)
+})
